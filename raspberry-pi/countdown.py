1a7d852928e058cac626a46baac033cacfc9eff3
CallanSmith <71345149+CallanSmith@users.noreply.github.com>
2022-10-21 10:16:43 -0400

    Create countdown.py

---
+++ raspberry-pi/countdown.py
@@ -0,0 +1,5 @@
+import time
+for x in range (10,0,-1):  #Range from 10,0 count down -1 each second
+    print(x)
+    time.sleep (1)  # Second between each number
+print("Takeoff!")   # print this at 0
