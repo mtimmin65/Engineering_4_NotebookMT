bce42b6687e650aff47b4339003fe4e9c1680903
CallanSmith <71345149+CallanSmith@users.noreply.github.com>
2022-10-21 10:35:19 -0400

    Update Countdown3.py

--- raspberry-pi/Countdown3.py
+++ raspberry-pi/Countdown3.py
@@ -7,19 +7,19 @@ GreenLed.direction = digitalio.Direction.OUTPUT
 RedLed = digitalio.DigitalInOut(board.GP18)
 RedLed.direction = digitalio.Direction.OUTPUT
 button = digitalio.DigitalInOut(board.GP16) 
-button.direction = digitalio.Direction.INPUT
+button.direction = digitalio.Direction.INPUT # Leds are output, Button is an Input
 button.pull = digitalio.Pull.UP 
 
 while True: 
-     if button.value == False:
-          for x in reversed(range(11)):
-               led1.value = True #light bright
-               time.sleep(0.5) #light snooze
-               print(x)
-               led1.value = False #light off
-               time.sleep(0.5)
-          while True:
-               print("liftoff!") #says dis
-               led2.value = True
-               time.sleep(0.5)
+     if button.value == False: # Button initializes code
+          for x in range (10,0,-1):  
+    GreenLed.value = True #Turns led on intially
+    time.sleep(0.5) # sleep half second
+    print(x) # continues  countdown
+    led1.value = False #turns led off
+    time.sleep(0.5) # sleep other half second
+     while True:
+         print("Takeoff!") 
+         RedLed.value = True #Red Led turns on at end of countdown
+         time.sleep(0.5) # Led turns off
 
