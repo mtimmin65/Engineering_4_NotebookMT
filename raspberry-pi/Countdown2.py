bedb9918308298c81c8136313a0b5662a8a9b3e5
CallanSmith <71345149+CallanSmith@users.noreply.github.com>
2022-10-21 10:27:24 -0400

    Create Countdown2.py

---
+++ raspberry-pi/Countdown2.py
@@ -0,0 +1,20 @@
+import time 
+import board
+import digitalio 
+
+GreenLed = digitalio.DigitalInOut(board.GP13)
+GreenLed.direction = digitalio.Direction.OUTPUT 
+RedLed = digitalio.DigitalInOut(board.GP18) 
+RedLed.direction = digitalio.Direction.OUTPUT 
+
+for x in range (10,0,-1):  
+    GreenLed.value = True #Turns led on intially
+    time.sleep(0.5) # sleep half second
+    print(x) # continues  countdown
+    led1.value = False #turns led off
+    time.sleep(0.5) # sleep other half second
+while True:
+    print("Takeoff!") 
+    RedLed.value = True #Red Led turns on at end of countdown
+    time.sleep(0.5) # Led turns off
+ 
